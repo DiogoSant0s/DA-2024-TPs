dd1302276871b31e7cd53bf64d10ac5aa80214f7
Diogo Santos <up202009291@up.pt>
2024-04-17 10:08:10 +0100

    pdfs

--- TP4/ex3.cpp
+++ TP4/ex3.cpp
@@ -12,6 +12,6 @@ std::string hanoiDC(unsigned int n, char src, char dest) {
 
 TEST(TP4_Ex3, testHanoi_height4) {
     std::string moves = hanoiDC(4, 'A', 'B');
-    EXPECT_EQ(std::count(moves.begin(),moves.end(),','), 14); // there are 15 moves
+    EXPECT_EQ(std::count(moves.begin(), moves.end(), ','), 14); // there are 15 moves
     EXPECT_EQ(moves,"A->C,A->B,C->B,A->C,B->A,B->C,A->C,A->B,C->B,C->A,B->A,C->B,A->C,A->B,C->B");
 }
